c0c84460340d6d48de4673179da36f5574161cd0
Bridget Unterreiner <bridget.unterreiner@gmail.com>
2018-03-13 22:27:56 -0600

    HW6 commit

--- HW-6/hw-6.js
+++ HW-6/hw-6.js
@@ -26,3 +26,55 @@ Instead, you should use the appropriate methods, as mentioned in the readings.
 To begin, you should select the body element from the DOM, then remove its existing child nodes. You may then begin to create your web page.
 
 */
+
+// Removing paragraphs
+var paragraphsToRemove = document.getElementsByTagName('p');
+var paragraphParent = paragraphsToRemove[0].parentNode;
+var parLength = paragraphsToRemove.length;
+
+for (var i = 0; i < parLength; i++) {
+    paragraphParent.removeChild(paragraphsToRemove[0]);
+    console.log(i);
+}
+
+// Content
+var siteTitleText = "BASIC SPACE";
+var par1Text = "I was recently made aware of the 'Basic Space' app through a fashion influencer I follow on Instagram. She was advertising different clothing and accessory items from her personal collection for sale on this app called 'Basic Space'. I am inspired by her style and was excited at the opportunity to purchase items from her personal wardrobe. I instantly went to the app store and downloaded the app... a prime example of the power of social media platforms and celebrity influence, am I right?!.";
+var par2Text = "I have experience with other resale apps such as Depop (a popular fashion-centric resale app available only for mobile), but was curious what made this one different. Upon downloading the app and taking a moment to explore, I realized that, unlike similar apps, 'Basic Space' only allowed specific people to sell items. On the 'sellers' page there was a variety of celebrities, fashion influencers, socialites, etc. that had profiles with items listed for sale. So, I can buy but can't sell on this app. While this air of exclusivity might turn some away, I found that it created an extremely enjoyable user experience. The app features a minimalistic design and the 'curated' nature of the app removes clutter and leaves buyers with quality and clarity when searching and buying.";
+
+// Creating and styling DOM nodes
+var siteTitle = document.createElement('h1');
+siteTitle.textContent = siteTitleText;
+siteTitle.style.color = "white";
+
+var par1 = document.createElement('p');
+par1.textContent = par1Text;
+par1.style.color = "white";
+
+var par2 = document.createElement('p');
+par2.textContent = par2Text;
+par2.style.color = "white";
+
+document.body.style.backgroundColor = "black";
+
+var img1 = document.createElement('img');
+img1.setAttribute('src', 'images/basic_space_app.png');
+img1.alignItems = "center"
+
+var linkWrapper = document.createElement('p');
+
+var link1 = document.createElement('a');
+link1.setAttribute('href', 'https://basic.space/');
+link1.textContent = 'CLICK HERE TO DOWNLOAD BASIC SPACE';
+link1.style.color = "white";
+
+linkWrapper.appendChild(link1);
+
+// Apply everything to DOM 
+var contentDiv = document.getElementsByTagName("div")[0];
+contentDiv.style.fontFamily = "sans-serif";
+contentDiv.appendChild(siteTitle);
+contentDiv.appendChild(par1);
+contentDiv.appendChild(par2);
+contentDiv.appendChild(linkWrapper);
+contentDiv.appendChild(img1);
--- HW-6/readme.md
+++ HW-6/readme.md
@@ -1,3 +1,7 @@
-# HW-6 Readme for YOUR_NAME
+# HW-6 Readme for BRIDGET UNTERREINER
 
-Please place your response to this week here.
+I was excited to bring things "back down to earth", so to speak, this week with a more practical application of the javascript skills we've been working on. Seeing our code represented as a physical webpage was rewarding and placed things in a context that allowed me to understand javascript in a deeper sense. 
+
+I definitely struggled with removing the child nodes from the DOM... but once I overcame this obstacle things began to make more sense. 
+
+Not being allowed to style my page using HTML or CSS was limiting and frustrating, but I am happy enough with my site's aesthetic considering the limitations. I aimed for an overall visual that complemented my site content.  
