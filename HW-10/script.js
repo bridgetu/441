1d62c7297e9509f354c2f0dcd91bfbdf9918300a
Bridget Unterreiner <bridget.unterreiner@gmail.com>
2018-05-09 20:32:09 -0600

    started at the bottom now we here

---
+++ HW-10/script.js
@@ -0,0 +1,8 @@
+var $grid = $('.grid').imagesLoaded( function() {
+    // init Masonry after all images have loaded
+    $grid.masonry({
+        columnWidth: 200,
+        itemSelector: '.grid-item',
+        gutter: 10
+    });
+  });
